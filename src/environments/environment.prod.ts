49a794a68c70453cb4fdeea15b64a143c2bb83fd
A.R.Z <lexz.zaldivar@gmail.com>
2022-01-15 15:50:28 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,10 @@
 // This is useful for granularity you might need beyond just the environment.
 // Note that as usual, any environment variables you expose through it will end up in your
 // bundle, and you should not use it for any sensitive information like passwords or keys.
-import { env } from './.env';
 
 export const environment = {
   production: true,
-  version: env.npm_package_version,
+  version: "0.0.1",
   serverUrl: 'https://api-gaceta.datalis.dev/api',
   newsApiUrl: 'https://api.eltoque.com/posts?categories=600c46c1929b80000d284502&_sort=publish_date:DESC&_limit=10',
   defaultLanguage: 'en-CU',
