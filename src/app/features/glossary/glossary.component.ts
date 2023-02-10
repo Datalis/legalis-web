8ee682901e1185f85aa9bdbe7f2c0676d2fe7599
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-02-09 22:37:39 -0800

    fix: always send non latin word on search

--- src/app/features/glossary/glossary.component.ts
+++ src/app/features/glossary/glossary.component.ts
@@ -1,14 +1,15 @@
 import { ApiService } from '@app/@shared/services/api.service';
 import { Params } from '@app/@shared/model/params';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, from, Observable, of, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { PagedResult } from './../../@shared/model/paged-result';
 //import { DataService } from '@app/@shared/services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { GlossaryTerm } from '@app/@shared/model/glossary-term';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { LETTERS } from '@app/@shared/utils/data';
+import deburr from 'lodash.deburr';
 
 @UntilDestroy()
 @Component({
@@ -38,9 +39,9 @@ export class GlossaryComponent implements OnInit {
   }
 
   loadMore() {
-    this.termsCount+=30;
+    this.termsCount += 30;
   }
-  
+
   constructor(private _apiService: ApiService, private _route: ActivatedRoute, private _router: Router) {
     this.references$ = this._apiService.getGlossaryRefs({});
     this.letters = LETTERS;
@@ -51,44 +52,17 @@ export class GlossaryComponent implements OnInit {
           this.params.startswith = params.startswith;
           return this._apiService.getGlossaryTerms({ ...params, page_size: 10000 });
         })
-      ).subscribe((res) => {
+      )
+      .subscribe((res) => {
         this.results = res;
-        console.log(res);
-      })
+        // console.log(res);
+      });
   }
 
   async ngOnInit() {
-
-
-    // this._route.queryParams.pipe(untilDestroyed(this)).subscribe(async (params) => {
-    //   // this.params.page = params.page;
-    //   // this.params.page_size = this.itemsPerPage;
-    //   this.params.startswith = params.startswith || this.defaultLetter;
-
-    //   const results = await this._apiService.getGlossaryTerms(this.params);
-    //   this.results = results;
-    // });
-
-
-
-    //this.letters = this._dataService.letters;
-    /*this._route.queryParams
-      .pipe(
-        tap(() => (this.isLoading = true)),
-        map((params) => Params.fromObject(params)),
-        switchMap((params) => {
-          this.params.page = params.page;
-          this.params.page_size = this.itemsPerPage;
-          this.params.startswith = params.startswith || this.defaultLetter;
-          return this._dataService.getGlossaryTerms(this.params);
-        })
-      )
-      .subscribe((res) => {
-        //this.results = res;
-        this.isLoading = false;
-      });*/
+    
   }
-  
+
   openRefs() {
     this.showRefs = true;
     this.params.startswith = null;
@@ -106,7 +80,7 @@ export class GlossaryComponent implements OnInit {
   }
 
   resetSearch(inputRef: HTMLInputElement) {
-    inputRef.value = ""
+    inputRef.value = '';
     this.params.search = null;
     this.params.startswith = this.defaultLetter;
     this.termCollapsed = -1;
@@ -115,7 +89,7 @@ export class GlossaryComponent implements OnInit {
 
   onSearchTerm(e: any) {
     let term = e.target.value;
-    this.params.search = term?.replace(/['"]+/g, '');
+    this.params.search = deburr(term);
     this.params.startswith = null;
     this.termCollapsed = -1;
     this.showRefs = false;
