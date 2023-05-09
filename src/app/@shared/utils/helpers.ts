f5b1baa796ea0289a61ecb448d2b4679ea830906
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-05-08 17:21:59 -0700

    serverless dev config

--- src/app/@shared/utils/helpers.ts
+++ src/app/@shared/utils/helpers.ts
@@ -1,5 +1,5 @@
 export const removeEmpty = (obj: any) => {
-  let newObj = {};
+  let newObj = <any>{};
   Object.keys(obj).forEach((key) => {
     if (obj[key] === Object(obj[key])) newObj[key] = removeEmpty(obj[key]);
     else if (obj[key] !== undefined && obj[key] !== null) newObj[key] = obj[key];
