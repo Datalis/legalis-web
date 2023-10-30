9bfaaa15d22cbfbb39e08809b4793781d9cdcc92
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-10-30 01:59:34 -0700

    feat: Add scroll to content to Preguntas Juridicas

--- src/app/features/analysis/analysis-detail/analysis-detail.component.ts
+++ src/app/features/analysis/analysis-detail/analysis-detail.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
+import { LayoutService } from '@app/@shared/services/layout.service';
 
 @Component({
   selector: 'app-analysis-detail',
   templateUrl: './analysis-detail.component.html',
   styleUrls: ['./analysis-detail.component.scss']
 })
-export class AnalysisDetailComponent implements OnInit, OnDestroy {
+export class AnalysisDetailComponent implements OnInit, OnDestroy, AfterViewInit {
 
   article: any = null;
   fq: any[] = [];
 
-  constructor(private _route: ActivatedRoute, private _meta: Meta) {
+  constructor(private _route: ActivatedRoute, private _meta: Meta, private _layoutService: LayoutService) {
     this._meta.addTag({ name: 'robots', content: 'noindex' });
   }
 
@@ -21,6 +22,18 @@ export class AnalysisDetailComponent implements OnInit, OnDestroy {
       this.article = data.data[0];
       this.fq = data.data[1] || [];
     });
+  }
+
+  ngAfterViewInit(): void {
+    this._route.url.subscribe(url => {
+      const fragment = this._route.snapshot.fragment;
+      if (fragment) {
+        this._layoutService.scrollToElement("#article-content")
+      } else {
+        this._layoutService.scrollToTop();
+      }
+    })
+
   }
 
   ngOnDestroy(): void {
