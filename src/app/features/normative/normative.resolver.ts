b1f72dc8071de8a2fd5141400f84d2b44799eb90
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2024-06-12 16:33:14 -0400

    (feat) Slugs for normatives

--- src/app/features/normative/normative.resolver.ts
+++ src/app/features/normative/normative.resolver.ts
@@ -1,23 +1,33 @@
-import { ApiService } from '@app/@shared/services/api.service';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
-import { Injectable } from '@angular/core';
+import { ApiService } from "@app/@shared/services/api.service";
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class NormativeResolver implements Resolve<any> {
-  constructor(private apiService: ApiService, private router: Router) {}
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const id = route.params.id;
-    if (!id) {
-      this.router.navigateByUrl('/');
+    const slug = route.params.slug;
+    if (!id && !slug) {
+      this.router.navigateByUrl("/");
       return Promise.reject();
-    } else {
-      return this.apiService.getNormative(id).then((normative) => {
-        const gazette = !!normative.gazette ? this.apiService.getGazette(normative.gazette) : Promise.resolve(null);
-        return Promise.all([Promise.resolve(normative), gazette]);
-      });
     }
+    return this.apiService.getNormative(id || slug).then((normative) => {
+      const gazette = !!normative.gazette
+        ? this.apiService.getGazette(normative.gazette)
+        : Promise.resolve(null);
+      return Promise.all([Promise.resolve(normative), gazette]);
+    });
   }
 }
--- src/app/features/normative/normative.routing.ts
+++ src/app/features/normative/normative.routing.ts
@@ -1,7 +1,18 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NormativeComponent } from './normative.component';
-import { NormativeResolver } from './normative.resolver';
+import { Routes, RouterModule } from "@angular/router";
+import { NormativeComponent } from "./normative.component";
+import { NormativeResolver } from "./normative.resolver";
 
-const routes: Routes = [{ path: ':id', component: NormativeComponent, resolve: { data: NormativeResolver } }];
+const routes: Routes = [
+  {
+    path: ":id",
+    component: NormativeComponent,
+    resolve: { data: NormativeResolver },
+  },
+  {
+    path: ":slug",
+    component: NormativeComponent,
+    resolve: { data: NormativeResolver },
+  },
+];
 
 export const NormativeRoutes = RouterModule.forChild(routes);
